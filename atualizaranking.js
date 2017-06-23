f533df72936f5d2b2119a9d9f15ceb9a4e36edc3
Rodrigo Gonçalves <rodrigo.rebes@gmail.com>
2017-06-23 02:02:55 -0300

    Ranking carrega junto com o documento

---
+++ atualizaranking.js
@@ -0,0 +1,17 @@
+$(document).ready(function(){
+	$.ajax({
+                url : "ranking.php",
+                dataType : "json",
+
+                success : function(resp){                 
+                    for (i = 0; i < resp.length; i++){
+                         $("table").prepend("<tr><td>" + resp[i].participante + "</td><td>" + resp[i].pontos + "</td><td>" + resp[i].scorefavor + "</td></tr>");
+                     }
+                     $("table").prepend("<tr><th>Participante</th><th>Pontuação</th><th>scorefavor</th></tr>");
+                },
+
+                error : function(err){
+                    alert("ERRO: " + err.status);
+                }
+            });
+});
---
+++ ranking.php
@@ -0,0 +1,11 @@
+<?php 
+	$con = pg_connect("host=localhost port=5432 dbname=Jogos user=postgres password=postgres");
+	$comando = "SELECT DISTINCT * FROM ranking ORDER BY pontos";
+	$res = pg_query($con, $comando);
+	$matriz = array();
+	while ($linha = pg_fetch_assoc($res)) {
+		$matriz[] = $linha;
+	}
+	echo json_encode($matriz);
+	pg_close($con);
+?>
